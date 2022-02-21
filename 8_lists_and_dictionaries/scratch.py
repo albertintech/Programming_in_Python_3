365a54f8a14ec09acb48cb95691600c69f2f8770
Albert Ramos <albertramos.dev@gmail.com>
2022-02-21 05:27:57 -0700

    Update file

--- 8_lists_and_dictionaries/scratch.py
+++ 8_lists_and_dictionaries/scratch.py
@@ -11,3 +11,25 @@ print(ltr_lst)
 print(ltr_lst[0])
 print(ltr_lst[1])
 print(ltr_lst[2])
+
+my_list = ['h', 'e', 'l', 'l', 'o']
+print(my_list)
+my_list[len(my_list):] = [' ', 'w', 'o', 'r', 'l', 'd', '.']
+print(my_list)
+my_list[11] = '!'
+print(my_list)
+del my_list[5]
+print(my_list)
+
+# my_teams = ['Raptors', 'Heat', 'Nets']
+# your_teams = my_teams  # Create a shared reference to same list
+# my_teams[1] = 'Lakers'  # Modify list element
+
+# print('My teams are:', my_teams)  # Both variables have changed
+# print('Your teams are:', your_teams)  # Both variables have changed
+
+my_teams = ['Raptors', 'Heat', 'Nets']
+your_teams = my_teams[:]  # Assign your_teams with a COPY of my_teams
+my_teams[1] = 'Lakers'  # Modify list element
+print('My teams are:', my_teams)  # List element has changed
+print('Your teams are:', your_teams)  # List element has not changed
