6bf245773e04de5eb19ce6441c2a47ba2a4c1cdb
Albert Ramos <albertramos.dev@gmail.com>
2022-02-20 20:20:35 -0700

    Add folder for Lesson 8

---
+++ 8_lists_and_dictionaries/scratch.py
@@ -0,0 +1,13 @@
+# Lists and Dictionaries
+
+last_alpha_lst = ['x', 'y', 'z']
+print(last_alpha_lst)
+print(last_alpha_lst[0])
+print(last_alpha_lst[1])
+print(last_alpha_lst[2])
+
+ltr_lst = list('abc')
+print(ltr_lst)
+print(ltr_lst[0])
+print(ltr_lst[1])
+print(ltr_lst[2])
